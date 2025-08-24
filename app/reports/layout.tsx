183dda9bfdc5568603539b2e18afd9d07693cb9a
Hrishikesh Patel <patelhrishikesh2000@gmail.com>
2025-08-24 06:10:10 +0530

    feat: Add Layout component with suspense for loading state

---
+++ app/reports/layout.tsx
@@ -0,0 +1,6 @@
+import type React from "react";
+import { Suspense } from "react";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>;
+}
