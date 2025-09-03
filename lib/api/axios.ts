8054972eab15bf624d33b525ba5d1e8937caf250
Hrishikesh Patel <patelhrishikesh2000@gmail.com>
2025-09-03 22:47:00 +0530

    fix: timeout in axios increased to 100000 instead of 10000;

--- lib/api/axios.ts
+++ lib/api/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "/api",
-  timeout: 10000,
+  timeout: 100000,
   headers: {
     "Content-Type": "application/json",
   },
